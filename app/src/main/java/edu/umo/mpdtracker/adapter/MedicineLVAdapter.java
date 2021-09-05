7901a48b0791de4738d3561658d315f9798e05ec
harshagiri <harshagiri.kumar@cerner.com>
2021-09-05 23:42:08 +0530

    Magnified Image Viewer
    Added magnified image viewer to see medicine photo after selecting the list item

--- app/src/main/java/edu/umo/mpdtracker/adapter/MedicineLVAdapter.java
+++ app/src/main/java/edu/umo/mpdtracker/adapter/MedicineLVAdapter.java
@@ -1,12 +1,21 @@
 package edu.umo.mpdtracker.adapter;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +24,7 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
+import edu.umo.mpdtracker.MainActivity;
 import edu.umo.mpdtracker.R;
 import edu.umo.mpdtracker.Model.Medicine;
 
@@ -38,7 +48,7 @@ public class MedicineLVAdapter extends ArrayAdapter<Medicine> {
          we are getting data from array list inside
          our modal class.
         */
-        Log.d("POSITION", position+"");
+        Log.d("POSITION", position + "");
         Medicine medicineDataModel = getItem(position);
 
         // initializing our UI components of list view item.
@@ -56,13 +66,36 @@ public class MedicineLVAdapter extends ArrayAdapter<Medicine> {
         // for our item of list view.
         listView.setClickable(true);
 
-        listView.setOnClickListener(new View.OnClickListener() {
+        Dialog settingsDialog = new Dialog(getContext());
+        settingsDialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        settingsDialog.setContentView(layoutInflater.inflate(R.layout.image_layout, null));
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        int height = (int) (displayMetrics.heightPixels - (displayMetrics.heightPixels * 0.2));
+        int width = displayMetrics.widthPixels;
+
+        settingsDialog.setOnShowListener(new DialogInterface.OnShowListener() {
             @Override
-            public void onClick(View v) {
-                // on the item click on our list view.
-                // we are displaying a toast message.
-                Toast.makeText(getContext(), "Item clicked is : " + medicineDataModel.getName(), Toast.LENGTH_SHORT).show();
+            public void onShow(DialogInterface dialog) {
+                ImageView medicineImageView = (ImageView) settingsDialog.findViewById(R.id.medicineImageViewer);
+                medicineImageView.setImageBitmap(medicineDataModel.getMedicinePhoto());
+
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams( width, height);
+                medicineImageView.setLayoutParams(layoutParams);
             }
+        });
+
+        Button dismissButton = (Button) settingsDialog.findViewById(R.id.dismissButton);
+        dismissButton.setOnClickListener(v -> settingsDialog.dismiss());
+
+        listView.setOnClickListener(v -> {
+            settingsDialog.show();
+            // on the item click on our list view.
+            // we are displaying a toast message.
+            Toast.makeText(getContext(), "Item clicked is : " + medicineDataModel.getName(), Toast.LENGTH_SHORT).show();
         });
         return listView;
     }
